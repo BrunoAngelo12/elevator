cf75c00605a8949f0ac81b5b6e76bf350a732bc0
Bruno Angelo <bruno.trabalhos@outlook.com>
2024-01-19 19:30:53 -0300

    criado a lógica de fazer o elevador subir

--- elevator/src/elevatorApp.java
+++ elevator/src/elevatorApp.java
@@ -1,5 +1,27 @@
-public class App {
+import java.util.Scanner;
+import java.util.Random;
+
+public class elevatorApp {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner sc = new Scanner(System.in);
+        Random floorRandom = new Random();
+        int currentFloor = floorRandom.nextInt(11);
+
+        System.out.println("\n----------Elevator Wall Street Tower-----------\n");
+        System.out.println("Current floor: " + currentFloor);
+        System.out.print("Enter your desired floor: ");
+        int selectedFloor = sc.nextInt();
+
+        if(selectedFloor > currentFloor){
+            for(int i =currentFloor;i <=selectedFloor;i++){
+                if(i == selectedFloor){
+                    System.out.println("Current floor: " + i + "\nBe careful with the door.");
+                    break;
+                }
+                System.out.println("Current floor: " + i + "\nGoing up..");
+            }
+        }
+
     }
 }
