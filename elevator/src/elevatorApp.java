5b712de1f0a1e18a19a1cdfc9c38f4a9f9249b3c
Bruno Angelo <bruno.trabalhos@outlook.com>
2024-01-19 19:56:03 -0300

    criado logica de descer e elevador no mesmo andar

--- elevator/src/elevatorApp.java
+++ elevator/src/elevatorApp.java
@@ -3,24 +3,39 @@ import java.util.Random;
 
 public class elevatorApp {
     public static void main(String[] args) throws Exception {
-        
+
         Scanner sc = new Scanner(System.in);
         Random floorRandom = new Random();
         int currentFloor = floorRandom.nextInt(11);
 
         System.out.println("\n----------Elevator Wall Street Tower-----------\n");
         System.out.println("Current floor: " + currentFloor);
-        System.out.print("Enter your desired floor: ");
-        int selectedFloor = sc.nextInt();
+        int selectedFloor;
+        do{
+            System.out.print("Enter your desired floor: ");
+            selectedFloor = sc.nextInt();
+        }while(selectedFloor == currentFloor || selectedFloor > 10 || selectedFloor < 0);
 
-        if(selectedFloor > currentFloor){
-            for(int i =currentFloor;i <=selectedFloor;i++){
-                if(i == selectedFloor){
+
+        if (selectedFloor > currentFloor) {
+            for (int i = currentFloor; i <= selectedFloor; i++) {
+                if (i == selectedFloor) {
                     System.out.println("Current floor: " + i + "\nBe careful with the door.");
                     break;
                 }
                 System.out.println("Current floor: " + i + "\nGoing up..");
             }
+        } else if (selectedFloor < currentFloor) {
+            for (int i = currentFloor; i >= selectedFloor; i--) {
+                if (i == selectedFloor) {
+                    System.out.println("Current floor: " + i + "\nBe careful with the door.");
+                    break;
+                }
+                System.out.println("Current floor: " + i + "\nGoing down..");
+            }
+        } else {
+            System.out.print("Enter your desired floor: ");
+            selectedFloor = sc.nextInt();
         }
 
     }
